aa3da99afec65982a306f40ca9f445bd88526d12
majidln <majidlotfinia12@gmail.com>
2020-10-11 23:30:25 +0330

    add manual language changer

--- src/App.js
+++ src/App.js
@@ -6,39 +6,17 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import {LanguageContextProvider} from './context/languageContext';
+import React from 'react';
+import { LanguageContextProvider } from './context/languageContext';
 import HomeScreen from './screen/home'
 
-function App()  {
+function App() {
 
   return (
     <LanguageContextProvider>
       <HomeScreen />
-      
     </LanguageContextProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1
-  },
-  contentWrapper: {
-    flex: 1,
-    alignItems: 'center', 
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 22
-  }
-});
 
 export default App;
--- src/screen/home.js
+++ src/screen/home.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { withTranslate } from './../context/languageContext'
 
-function Home({ translate }) {
+function Home({ translate, setLanguage }) {
+    const changeLang = (lang = 'en') => {
+        setLanguage(lang)
+    }
+
     return (
         <SafeAreaView style={styles.wrapper}>
-            <Text style={styles.text}>
-                {translate.welcome}
-            </Text>
+            <View style={styles.contentWrapper}>
+                <Text style={styles.text}>
+                    {translate.welcome}
+                </Text>
+            </View>
+            <View style={styles.operationWrapper}>
+                <TouchableOpacity onPress={() => changeLang('fa-IR')} style={styles.button}>
+                    <Text style={styles.buttonText}>Fa</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => changeLang('en')} style={styles.button}>
+                    <Text style={styles.buttonText}>EN</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
 
     )
@@ -16,12 +30,28 @@ function Home({ translate }) {
 const styles = StyleSheet.create({
     wrapper: {
         flex: 1,
+    },
+    contentWrapper: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
     text: {
         textAlign: 'center',
         fontSize: 22
+    },
+    operationWrapper: {
+        flexDirection: 'row',
+    },
+    button: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'gray'
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: 'center',
+        padding: 20
     }
 })
 
